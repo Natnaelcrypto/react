3246d5d6e13082558821a04ec67169c1940f69c2
Natnaelcrypto <120390910+Natnaelcrypto@users.noreply.github.com>
2023-01-01 15:43:02 +0300

    Update posts.js

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"mongodb+srv://Natnael:<password>@cluster0.idbugid.mongodb.net/?retryWrites=true&w=majority"
+    baseURL:"mongodb+srv://Natnael:<26031994n>@cluster0.idbugid.mongodb.net/?retryWrites=true&w=majority"
 })
